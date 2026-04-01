d7b1e23f01d4dabd4e46e727db3cf929dd4e42da
Theseas Maroulis <info@theseas.eu>
2026-04-02 00:18:58 +0300

    refactor: disabled dir walking and only handle a single file for now

--- fwatch.c
+++ fwatch.c
@@ -1,28 +1,33 @@
+#include <stdint.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <ftw.h>
 #include <fnmatch.h>
+#include <fcntl.h>
 
 struct Arguments {
-	char *pattern;
+	char *filename;
 	char *command;
 } arguments;
 
-int handle_file(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
-	// printf("%lu: %s\n", sb->st_ino, fpath);
-	// see fnmatch
-	
-	if (fnmatch(arguments.pattern, fpath, 0) != FNM_NOMATCH) {
-		printf(" * %s matches %s\n", fpath, arguments.pattern);
-	}
-	return 0;
-}
+// int handle_file(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+// 	// printf("%lu: %s\n", sb->st_ino, fpath);
+// 	// see fnmatch
+//
+// 	if (fnmatch(arguments.pattern, fpath, 0) != FNM_NOMATCH) {
+// 		printf(" * %s matches %s\n", fpath, arguments.pattern);
+// 	}
+// 	return 0;
+// }
 
 
 int main(int argc, char* argv[]) {
+	struct stat file_stat = {0};
+
 	// quick and dirty version
 	// TODO: handle various errors and unsafe handlings
 	if (argc != 3) {
@@ -30,15 +35,29 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 	printf("1: %s\n2: %s\n", argv[1], argv[2]);
-	arguments.pattern = argv[1];
+	arguments.filename = argv[1];
 	arguments.command = argv[2];
 	
-	size_t buffer_size = 8 * 1024 * 1024;
-	char *cwd = malloc(buffer_size);
-	getcwd(cwd, buffer_size-1);
-	printf("%s\n", cwd);
+	if (lstat(arguments.filename, &file_stat) == -1) {
+		perror("lstat");
+		return EXIT_FAILURE;
+	}
+	printf("file: %s -> %jo\n", arguments.filename, (uintmax_t)file_stat.st_mode);
 
-	nftw(cwd, handle_file, 1024, FTW_F);
-	free(cwd);
-	return 0;
+	if (file_stat.st_mode & S_IFREG) {
+		printf("Regural file\n");
+	}
+
+	if (file_stat.st_mode & S_IFDIR) {
+		// TODO: implement it
+		printf("Directory\n");
+	}
+	// size_t buffer_size = 8 * 1024 * 1024;
+	// char *cwd = malloc(buffer_size);
+	// getcwd(cwd, buffer_size-1);
+	// printf("%s\n", cwd);
+
+	// nftw(cwd, handle_file, 1024, FTW_F);
+	// free(cwd);
+	return EXIT_SUCCESS;
 }
